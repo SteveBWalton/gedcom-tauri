995533674a975c833d44286a8c8722737ba07be5
Steve Walton <37951256+SteveBWalton@users.noreply.github.com>
2024-03-30 13:12:35 +0000

    Added an action and a button.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-static global_int: i32 = 5;
+static mut global_int: i32 = 5;
 
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
+    .invoke_handler(tauri::generate_handler![greet, test_action])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
@@ -14,5 +14,24 @@ fn main() {
 
 #[tauri::command]
 fn greet(name: &str) -> String {
-   format!("Hello, {name} {global_int}!")
+    let mut result: String = "".to_string();
+    unsafe {
+        global_int += 1;
+        result = format!("Hello, {name} {global_int}!");
+    }
+    println!("greet() has executed.");
+    return result;
 }
+
+
+
+#[tauri::command]
+fn test_action() {
+    unsafe {
+        global_int += 1;
+    }
+    println!("test_action() has executed.");
+}
+
+
+
